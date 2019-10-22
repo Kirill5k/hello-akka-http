deb0432a7a1db58249f5961a07a0ec92256cd04e
kirill5k <immotional@aol.com>
2019-10-22 20:00:11 +0100

    json marshalling with high level api

--- src/main/scala/io/kirill/helloakkahttp/highlevel/JsonMarshalling.scala
+++ src/main/scala/io/kirill/helloakkahttp/highlevel/JsonMarshalling.scala
@@ -4,7 +4,15 @@ import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.stream.ActorMaterializer
 import GameAreaMap._
 import akka.http.scaladsl.Http
-import io.kirill.helloakkahttp.highlevel.Server2.serverRoute
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.StatusCodes
+import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import spray.json._
 
 case class Player(nickname: String, characterClass: String, level: Int)
 
@@ -13,7 +21,7 @@ object GameAreaMap {
   case class GetPlayer(nickname: String)
   case class GetPLayerByClass(characterClass: String)
   case class AddPlayer(player: Player)
-  case class RemovePlayer(player: Player)
+  case class RemovePlayer(nickname: String)
   case object OperationSuccess
 }
 
@@ -33,17 +41,21 @@ class GameAreaMap extends Actor with ActorLogging {
       log.info(s"adding new player $player")
       players = players + (player.nickname -> player)
       sender ! OperationSuccess
-    case RemovePlayer(player) =>
-      log.info(s"removing player $player")
-      players = players - player.nickname
+    case RemovePlayer(nickname) =>
+      log.info(s"removing player $nickname")
+      players = players - nickname
       sender ! OperationSuccess
   }
 }
 
+trait PlayerJsonProtocol extends DefaultJsonProtocol {
+  implicit val playerJsonFormat = jsonFormat3(Player)
+}
 
-object JsonMarshalling extends App {
+object JsonMarshalling extends App with PlayerJsonProtocol with SprayJsonSupport {
   implicit val system = ActorSystem("json-marshalling")
   implicit val materializer = ActorMaterializer()
+  implicit val defaultTimeout = Timeout(2 seconds)
   import system.dispatcher
   import akka.http.scaladsl.server.Directives._
 
@@ -58,20 +70,29 @@ object JsonMarshalling extends App {
   val gameServerRoute = pathPrefix("api" / "players") {
     get {
       (path(Segment) | parameter("nickname".as[String])) { nickname =>
-        reject
+        val player = (game ? GetPlayer(nickname)).mapTo[Option[Player]]
+        complete(player)
       } ~
       parameter("class".as[String]) { characterClass =>
-        reject
+        val playerByClassFuture = (game ? GetPLayerByClass(characterClass)).mapTo[List[Player]]
+        complete(playerByClassFuture)
       } ~
       pathEndOrSingleSlash {
-        reject
+        val players = (game ? GetAllPlayers).mapTo[List[Player]]
+        complete(players)
       }
     }~
     post {
-      reject
+      entity(as[Player]) { player =>
+        val response = (game ? AddPlayer(player)).map(_ => StatusCodes.Created)
+        complete(response)
+      }
     } ~
     delete {
-      reject
+      path(Segment) { nickname =>
+        val response = (game ? RemovePlayer(nickname)).map(_ => StatusCodes.NoContent)
+        complete(response)
+      }
     }
   }
 
