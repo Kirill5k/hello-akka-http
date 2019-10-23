0bb2b2d5c9a63918b841bdf5551b5ebafcbdc717
kirill <immotional@aol.com>
2019-10-23 08:11:12 +0100

    rejections handling and custom handlers

---
+++ src/main/scala/io/kirill/helloakkahttp/highlevel/Rejections.scala
@@ -0,0 +1,67 @@
+package io.kirill.helloakkahttp.highlevel
+
+import akka.actor.ActorSystem
+import akka.http.javadsl.server.MethodRejection
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.{MissingQueryParamRejection, Rejection, RejectionHandler}
+import akka.stream.ActorMaterializer
+
+object Rejections extends App {
+  implicit val system = ActorSystem("rejections")
+  implicit val materializer = ActorMaterializer()
+
+  import system.dispatcher
+  import akka.http.scaladsl.server.Directives._
+
+  val route = pathPrefix("api" / "endpoint") {
+    get {
+      path(IntNumber) { id =>
+        complete(StatusCodes.OK)
+      } ~
+        pathEndOrSingleSlash {
+          complete(StatusCodes.OK)
+        }
+    }
+  }
+
+  // Rejection handlers
+  val badRequestHandler: RejectionHandler = { rejections: Seq[Rejection] =>
+    println(s"encountered rejections: $rejections")
+    Some(complete(StatusCodes.BadRequest))
+  }
+
+  val forbiddenHandler: RejectionHandler = { rejections: Seq[Rejection] =>
+    println(s"encountered rejections: $rejections")
+    Some(complete(StatusCodes.Forbidden))
+  }
+
+  val routeWithHandlers = handleRejections(badRequestHandler) {
+    route ~
+      pathPrefix("api" / "another-endpoint") {
+        post {
+          handleRejections(forbiddenHandler) {
+            parameter("param") { _ =>
+              complete(StatusCodes.OK)
+            }
+          }
+        }
+      }
+  }
+
+  implicit val customRejectionHandler = RejectionHandler.newBuilder()
+    .handle {
+      case m: MethodRejection =>
+        println(s"received method rejection $m")
+        complete("Rejected method!")
+    }
+    .handle {
+      case m: MissingQueryParamRejection =>
+        println(s"query param rejection $m")
+        complete("Rejected query param!")
+    }
+    .result()
+
+//  Http().bindAndHandle(routeWithHandlers, "localhost", 8080)
+  Http().bindAndHandle(route, "localhost", 8080)
+}
